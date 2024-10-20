fbaf15d1ac5a370875a54b46db1d61d58636b30e
Tine Lüttgen <christine.luettgen@gmail.com>
2024-10-20 13:33:06 +0200

    updated HTML in APP

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tine Luettgen and is{" "}
-        <a
-          href="https://github.com/Heimatseiten/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Tine Luettgen and is{" "}
+          <a
+            href="https://github.com/Heimatseiten/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
