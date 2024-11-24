5066b18107c5212680b3ff4e2935b530c01d2a80
Tine Lüttgen <christine.luettgen@gmail.com>
2024-11-24 19:19:28 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,8 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "667d9f573c8af4c33457be5d561a9148";
+
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
