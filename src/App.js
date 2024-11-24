79472b99fc4a274013fe7937eac4ed2ef8d749dc
Tine Lüttgen <christine.luettgen@gmail.com>
2024-11-24 18:58:25 +0100

    Refresh

--- src/App.js
+++ src/App.js
@@ -6,15 +6,28 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Cologne" />
+        <Weather defaultCity="New York" />
+
         <footer>
-          This project was coded by Tine Luettgen and is{" "}
+          This project was coded by{" "}
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            Tine Lüttgen
+          </a>{" "}
+          and is{" "}
           <a
             href="https://github.com/Heimatseiten/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            open sourced on GitHub
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://symphonious-entremet-0d657b.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -32,8 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    let apiKey = "667d9f573c8af4c33457be5d561a9148";
-
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
